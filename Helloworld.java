748d772c866b27bd7d4321e76394b38c2fce4145
rita <your_email@example.com>
2025-04-11 23:58:48 +0800

    fix bug 10.04.2025

--- Helloworld.java
+++ Helloworld.java
@@ -3,5 +3,12 @@ public class Helloworld {
     System.out.println("Hello World! I am Rita Lam.");
     System.out.println("Goodbye everyone.");
     
+    
+  }
+}
+public class Helloworld {
+  public static void main (String[] args) {
+    System.out.println(x:"Hello everyone!");
+
   }
 }
